292ec31fd3cf78312432057acc013ac8258f3f7d
Dan Prokopiwskyi <dan.prokopiwskyi@digital.hmrc.gov.uk>
2019-08-07 16:56:32 +0100

    PLATUI-166 updated the server to maintain a list of captured URLs, and to write the data file to disk.

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const app = express()
 const port = 6001
 
 app.use(express.json({limit: '500mb',}));
+var capturedUrls = [];
+var excludeUrls = ["http://localhost:9949/auth-login-stub/gg-sign-in"]
 
 app.post('/page-data', (req, res) => {
   const body = req.body;
@@ -14,20 +16,24 @@ app.post('/page-data', (req, res) => {
   logData.pageHTML = logData.pageHTML.substr(0, 100) + '...'
   logData.files = Object.keys(logData.files)
 
-  const fileList = Object.assign({}, body.files, {'index.html': '<!DOCTYPE html>\n' + body.pageHTML})
-
-  fs.mkdirSync(rootDir)
-
-  Object.keys(fileList).forEach(fileName => {
-    fs.writeFile(path.join(rootDir, fileName), fileList[fileName], (err, data) => {
-      if (err) {throw err}
-      console.log('saved file', fileName, body.pageHTML.substr(0, 20) + '...')
+  if(!capturedUrls.includes(body.pageURL) && !excludeUrls.includes(body.pageURL)) {
+    capturedUrls.push(body.pageURL)
+    const fileList = Object.assign({}, body.files, {'index.html': '<!DOCTYPE html>\n' + body.pageHTML}, {'data': body.pageURL})
+    fs.mkdirSync(rootDir)
+    Object.keys(fileList).forEach(fileName => {
+      fs.writeFile(path.join(rootDir, fileName), fileList[fileName], (err, data) => {
+        if (err) {throw err}
+        console.log('saved file', fileName, body.pageHTML.substr(0, 20) + '...')
+      })
     })
-  })
-
+  }
   console.log(logData)
 
   res.status('201').send('Done')
+})
+
+app.get('/urls', (req, res) => {
+  res.status(200).send(capturedUrls)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
