5821d196ac0310fc99bda794ce0dbde2888bbbd9
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2019-08-16 13:18:54 +0100

    Removed test.suite.location property from shell script.

--- generate-alert-data.sh
+++ generate-alert-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # generate the report
-(cd a11y-report-parser;sbt -Dtest.suites.location=/Users/ejayaraman/Workspace/EE/HMRC/automated-a11y-testing/pages "run-main uk.gov.hmrc.a11y.ReportParser";mv report* ../)
+(cd a11y-report-parser;sbt "run-main uk.gov.hmrc.a11y.ReportParser";mv report* ../)
