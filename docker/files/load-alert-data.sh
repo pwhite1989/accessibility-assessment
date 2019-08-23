1a3acce2bc4cb6e1c4078d8286d3e74c277c5083
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2019-08-23 11:55:10 +0100

    PLATUI-182: testRunTimeStamp is now a DateTime field in ElastiSearch/Kibana Added new Kibana Json object with Dashboards Updated process-job.sh to mount volumes a11y-report-parser and load-alert-data.sh

--- docker/files/load-alert-data.sh
+++ docker/files/load-alert-data.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-curl -H 'Content-Type: application/x-ndjson' -XPOST "${ELS_BULK_UPLOAD_API}" --data-binary @${1}
+curl -I "${ELS_HOST}/${ELS_INDEX}" | grep 200
+
+if [ $? -eq 1 ]
+then
+  echo "Elastic Seearch Index not found. Creating Index: ${ELS_INDEX}"
+  curl -X PUT "${ELS_HOST}/${ELS_INDEX}" \
+  -H 'Content-Type: application/json' \
+  -d '{"mappings": {"alerts": { "properties": {  "testRun":  {"type":   "date", "format": "date_time"}}}}}'
+else
+  echo "Elastic Seearch Index: ${ELS_INDEX} already exists. Skipping Index creation."
+fi
+
+curl -H 'Content-Type: application/x-ndjson' -X POST "${ELS_BULK_UPLOAD_API}" --data-binary @${1}
