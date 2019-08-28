d737ae4de9c689afd7d82e5e19d8a366900c6971
Dan Prokopiwskyi <dan.prokopiwskyi@digital.hmrc.gov.uk>
2019-08-28 10:43:33 +0100

    Updated process script to remove bulk upload files from report parser

--- docker/process-job.sh
+++ docker/process-job.sh
@@ -2,6 +2,8 @@
 
 # Set JENKINS_USERNAME and JENKINS_API_KEY as local environment variables.
 
+rm -f ../a11y-report-parser/bulk-upload-*
+
 ################################################################################
 # Use the following command for testing a11y-parser, entry_point and
 # assessAllPages script changes locally
@@ -28,5 +30,3 @@ docker run --cpus 3  \
 #    -e JENKINS_BASE_URI= \
 #    -e JENKINS_TEST_FOLDER= \
 #    accessibility-assessment:1.0.0
-
-#docker run -v $(pwd)/files/entry_point.sh:/opt/scripts/entry_point.sh -v $(pwd)/files/load-alert-data.sh:/home/seluser/load-alert-data.sh -v $(pwd)/../a11y-report-parser:/home/seluser/a11y-report-parser -e TEST_SUITE_NAME=${1} -e JENKINS_USERNAME=${JENKINS_USERNAME} -e JENKINS_API_KEY=${JENKINS_API_KEY} accessibility-assessment:1.0.0
