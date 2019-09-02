68c291ca0930ef9ddf9d87c9cf8bb6771c35ba2e
Dan Prokopiwskyi <dan.prokopiwskyi@digital.hmrc.gov.uk>
2019-09-02 11:17:00 +0100

    PLATUI-132 updated name and location of report parser.  Updated entry_point script, removing reference to the 'server' directory

--- docker/files/entry_point.sh
+++ docker/files/entry_point.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 
-# Retrieve a zip file from the last successful build.
+# Retrieve the zip and extract contents
 test_suite_url=$(curl -s --user ${JENKINS_USERNAME}:${JENKINS_API_KEY} ${JENKINS_BASE_URI}/${JENKINS_TEST_FOLDER}/a11y-test-${TEST_SUITE_NAME}/api/json | jq '.lastSuccessfulBuild | .url' | tr -d '"')
-
-export JENKINS_ARTIFACT_LOCATION=${test_suite_url}artifact/automated-a11y-testing/server
-curl -L -o ${TEST_SUITE_NAME}.zip --user ${JENKINS_USERNAME}:${JENKINS_API_KEY} "${JENKINS_ARTIFACT_LOCATION}"/*zip*/server.zip
-
-# Unzip the file - restructure files/directories to suite assessAllPages.sh processing and serving of pages via jenkins:
-# TODO:  We may want to update Jenkins and the assessAllPages.sh script rather than have this here...
+export JENKINS_ARTIFACT_LOCATION=${test_suite_url}artifact/automated-a11y-testing
+curl -L -o ${TEST_SUITE_NAME}.zip --user ${JENKINS_USERNAME}:${JENKINS_API_KEY} "${JENKINS_ARTIFACT_LOCATION}"/*zip*/output.zip
 unzip -q ${TEST_SUITE_NAME}.zip -d test-suites/${TEST_SUITE_NAME}
-cd test-suites/${TEST_SUITE_NAME}
-cp -r server/output output
-rm -rf server/
 
-# TODO: Refactor assessAllPages.sh to run against a given directory (first param...)
+# Run the assessment
 cd ${HOME}/test-suites
 source assessAllPages.sh ${TEST_SUITE_NAME}
 
 # Run the report parser
-cd ~/a11y-report-parser
 java -Dtest.suites.location="${HOME}/test-suites" -jar a11y-report-parser-assembly-0.1.jar
 
 # Find the bulk upload file and load it to the given ELS instance
