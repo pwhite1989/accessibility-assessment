8b853faac4a19bc337e4ed44e4d4167807d1e04b
Dan Prokopiwskyi <dan.prokopiwskyi@digital.hmrc.gov.uk>
2019-09-03 15:48:34 +0100

    PLATUI-132 refactored accessibility-assessment image.  Updated parser.

--- docker/files/entry_point.sh
+++ docker/files/entry_point.sh
@@ -2,13 +2,15 @@
 
 # Retrieve the zip and extract contents
 test_suite_url=$(curl -s --user ${JENKINS_USERNAME}:${JENKINS_API_KEY} ${JENKINS_BASE_URI}/${JENKINS_TEST_FOLDER}/a11y-test-${TEST_SUITE_NAME}/api/json | jq '.lastSuccessfulBuild | .url' | tr -d '"')
-export JENKINS_ARTIFACT_LOCATION=${test_suite_url}artifact/automated-a11y-testing
-curl -L -s -o ${TEST_SUITE_NAME}.zip --user ${JENKINS_USERNAME}:${JENKINS_API_KEY} "${JENKINS_ARTIFACT_LOCATION}"/*zip*/output.zip
+export JENKINS_ARTIFACT_LOCATION=${test_suite_url}artifact/page-capture-service
+curl -L -s -o ${TEST_SUITE_NAME}.zip --user ${JENKINS_USERNAME}:${JENKINS_API_KEY} "${JENKINS_ARTIFACT_LOCATION}"/*zip*/page-capture-service.zip
 unzip -q ${TEST_SUITE_NAME}.zip -d test-suites/${TEST_SUITE_NAME}
 
 # Run the assessment
-cd ${HOME}/test-suites
 source assessAllPages.sh ${TEST_SUITE_NAME}
 
 # Run the report parser
-java -Dtest.suites.location="${HOME}/test-suites" -jar accessibility-assessment-report-parser.jar
+java -Dtest.suite.name="${TEST_SUITE_NAME}" \
+     -Dtest.suite.file.location="${HOME}/test-suites/${TEST_SUITE_NAME}/page-capture-service/output" \
+     -Dtest.suite.artefact.location="$JENKINS_ARTIFACT_LOCATION/output" \
+     -jar accessibility-assessment-report-parser.jar
--- test-assessment-image.sh
+++ test-assessment-image.sh
@@ -2,18 +2,21 @@
 
 # Set JENKINS_USERNAME and JENKINS_API_KEY as local environment variables.
 
-rm -f $(pwd)/files/output/*
+rm -f $(pwd)/docker/files/output/*
+docker rm a11y
 
 ################################################################################
 # Use the following command for testing entry_point, load-alert-data.sh and
 # assessAllPages script changes locally.
 ################################################################################
 docker run --cpus 3  \
-    -v $(pwd)/docker/files/entry_point.sh:/opt/scripts/entry_point.sh \
-    -v $(pwd)/docker/files/assessAllPages.sh:/home/seluser/test-suites/assessAllPages.sh \
-    -v $(pwd)/docker/files/load-alert-data.sh:/home/seluser/load-alert-data.sh \
+    --name a11y \
     -v $(pwd)/docker/files/output:/home/seluser/output \
     -e TEST_SUITE_NAME=${1} \
     -e JENKINS_USERNAME=${JENKINS_USERNAME} \
     -e JENKINS_API_KEY=${JENKINS_API_KEY} \
     accessibility-assessment:1.0.0
+
+#-v $(pwd)/docker/files/accessibility-assessment-report-parser.jar:/home/seluser/accessibility-assessment-report-parser.jar \
+#-v $(pwd)/docker/files/entry_point.sh:/home/seluser/entry_point.sh \
+#-v $(pwd)/docker/files/assessAllPages.sh:/home/seluser/assessAllPages.sh \
