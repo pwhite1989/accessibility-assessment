6361d7cbc0cbe950f5eaa49342335286bc960755
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2020-01-20 16:45:59 +0000

    PLATUI-370: Included the new version of page-accessibility-check.jar which generates HTML and JSON report

--- docker/files/entry_point.sh
+++ docker/files/entry_point.sh
@@ -32,6 +32,7 @@ PARSER_OUTPUT="$(java -Dtest.suite.name="${test_suite_name}" \
      -Dtest.suite.file.location="${HOME}/pages" \
      -Dtest.suite.artefact.location="${artefact_location}" \
      -Dconfig.file="${HOME}/global-filters.conf" \
+     -Dtest.suite.build.url="${BUILD_URL}" \
      -jar page-accessibility-check.jar 2>&1)"
 
 if [ $? -eq 0 ]
