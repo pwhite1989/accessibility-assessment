5f299deb38c6a79a820d7fc28c3704385c591296
Dan Prokopiwskyi <dan.prokopiwskyi@digital.hmrc.gov.uk>
2020-03-10 14:58:18 +0000

    PLATUI-368 moved kibana apps to test folder. moved test-assessment-image.sh script to scripts/

---
+++ scripts/test-assessment-image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf $(pwd)/docker/files/output/*
+docker rm a11y
+
+################################################################################
+# Use the following command for testing entry_point, load-alert-data.sh and
+# assessAllPages script changes locally.
+################################################################################
+docker run --cpus 3  \
+    --name a11y \
+    -v $(pwd)/docker/files/output:/home/seluser/output \
+    -v $(pwd)/docker/files/app:/home/seluser/app \
+    -v $(pwd)/docker/files/application.conf:/home/seluser/application.conf \
+    -p 6010:6010 \
+    accessibility-assessment:SNAPSHOT
+
+# -v $(pwd)/docker/files/run_assessment.sh:/home/seluser/run_assessment.sh \
