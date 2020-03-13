4e801a9c9c92912cf5df766eb3dd841af0d26dbf
Dan Prokopiwskyi <dan.prokopiwskyi@digital.hmrc.gov.uk>
2020-03-13 12:09:49 +0000

    PLATUI-368 remove the app/ directory.

--- docker/files/app/resources/generate_report.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd ${HOME}/report
-cat ${HOME}/output/accessibility-assessment-report.json | ./prepare-report.js > ${HOME}/output/accessibility-assessment-report.html
--- docker/files/app/routes/status.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const logger = require('../logger')
-
-router.get('/', (req, res) => {
-  logger.log("INFO",`Returning Accessibility assessment service status:${global.status}`)
-  res.status(200).send(global.status)
-})
-
-module.exports = router;
