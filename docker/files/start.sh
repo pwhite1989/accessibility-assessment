94616e58c160619c15f62d9a0a2dd1c79aa83a2b
Dan Prokopiwskyi <1702422+dannyprok@users.noreply.github.com>
2020-06-30 14:50:55 +0100

    PLATUI-560 upgrade to base image, axe and vnu versions.  Removed reference to chrome-driver setup in Dockerfile

--- docker/files/start.sh
+++ docker/files/start.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+
+# Set the axe-core version for the report
+cd ${HOME}/.nvm/versions/node/v${NODE_VERSION}/lib/node_modules/axe-cli
+export NPM_AXE_VERSION=$(npm ls | grep axe-core | head -1 | awk -F@ '{print $2}') \
+
+# Start the application
 cd ${HOME}/app
 export NODE_ENV=docker && node app.js
